5986681028341fdc8584f59e6bf404e8c7ff2124
Christopher Stevens <obi_wan02@hotmail.com>
2021-09-08 15:15:43 +0100

    Initial Premake and gitignore

---
+++ premake5.lua
@@ -0,0 +1,48 @@
+project "yaml-cpp"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++17"
+    staticruntime "on"
+    warnings "extra"
+
+    configureProjectProperties("%{prj.name}")
+
+    files
+    {
+        "src/**.h",
+        "src/**.cpp",
+
+        "include/**.h",
+    }
+
+    includedirs
+    {
+        "include"
+    }
+
+
+    filter { "action:xcode*" }
+        XCodeBuildSettings =
+        {
+        }
+        merge(XCodeBuildSettings, OS.XCodeBuildSettings)
+        xcodebuildsettings (XCodeBuildSettings)
+
+    filter "system:windows"
+        systemversion "latest"
+        cppdialect "C++17"
+        staticruntime "On"
+
+    filter "system:linux"
+        pic "On"
+        systemversion "latest"
+        cppdialect "C++17"
+        staticruntime "On"
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
